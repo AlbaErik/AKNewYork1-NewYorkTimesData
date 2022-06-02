6ea5add92645b450f9e6628f43f6ebccb09a4e70
AlbaErik <erik.alba97@gmail.com>
2022-06-02 11:22:04 -0300

    Revert "Corregida ubicacion de la constante URL_NYTUMES_LOGO"
    This reverts commit d028b18947bd4ca85d73a1675644feb80433eb79.

--- src/main/java/ayds/ak1/newyorktimes/article/external/infos/NYTimesToInfoResolver.kt
+++ src/main/java/ayds/ak1/newyorktimes/article/external/infos/NYTimesToInfoResolver.kt
@@ -3,7 +3,6 @@ package ayds.ak1.newyorktimes.article.external.infos
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import ayds.ak1.newyorktimes.article.external.NYArticle
-import ayds.ak1.newyorktimes.article.external.URL_NYTIMES_LOGO
 
 interface NYTimesToInfoResolver {
     fun getArtistInfoFromExternalData(serviceData: String?, artistName: String): NYArticle?
@@ -13,6 +12,7 @@ private const val RESPONSE = "response"
 private const val DOCS = "docs"
 private const val ABSTRACT = "abstract"
 private const val WEB_URL = "web_url"
+private const val URL_NYTIMES_LOGO="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/NYC_Montage_2014_4_-_Jleon.jpg/456px-NYC_Montage_2014_4_-_Jleon.jpg"
 
 internal class JsonToInfoResolver : NYTimesToInfoResolver {
 
