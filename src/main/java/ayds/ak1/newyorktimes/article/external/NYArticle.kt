d028b18947bd4ca85d73a1675644feb80433eb79
AlbaErik <erik.alba97@gmail.com>
2022-06-02 11:21:48 -0300

    Corregida ubicacion de la constante URL_NYTUMES_LOGO

--- src/main/java/ayds/ak1/newyorktimes/article/external/NYArticle.kt
+++ src/main/java/ayds/ak1/newyorktimes/article/external/NYArticle.kt
@@ -1,9 +1,11 @@
 package ayds.ak1.newyorktimes.article.external
 
+const val URL_NYTIMES_LOGO =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/NYC_Montage_2014_4_-_Jleon.jpg/456px-NYC_Montage_2014_4_-_Jleon.jpg"
 
 data class NYArticle(
     val description: String,
     val infoURL: String,
-    val artistName:String,
+    val artistName: String,
     val logoURL: String
 )
